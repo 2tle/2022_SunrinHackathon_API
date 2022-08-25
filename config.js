6ece52dc168a03fa9709987cb689d2ce675c6a8a
Hyunjun Yang <sw4019@naver.com>
2022-08-25 17:14:19 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "secret" : "A#@aB32C$dEF^G**H",
-  "mongodb": "mongodb+srv://hacker:hacker0101@cluster0.cniebvk.mongodb.net/?retryWrites=true&w=majority",
+  "secret" : "",
+  "mongodb": "",
   "port": 3000,
-  "hostname":"2022API.2tle.repl.co"
+  "hostname":""
 }
